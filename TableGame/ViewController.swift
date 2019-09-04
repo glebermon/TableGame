74450561b4a98cae349a480a74e890f6b9c2a18a
Gleb <monetchikov.gleb@icloud.com>
2019-09-04 08:51:28 +0300

    Initial Commit

---
+++ TableGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableGame
+//
+//  Created by Глеб on 04/09/2019.
+//  Copyright © 2019 Глеб. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
